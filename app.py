ad6f7929e7af65d1098a561890cfbaabe5cd17ff
Benjamin Pearce <bpearce9@gatech.edu>
2016-05-01 04:09:18 -0400

    updated formatting for header

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ class FullscreenWindow:
             direction = "Southbound"
 
         self.header = Label(self.frame,
-            text=stop + " - " + direction,
+            text=stop + " - \n" + direction,
             font=("Helvetica", 40, "bold"))
         self.header.pack()
 
